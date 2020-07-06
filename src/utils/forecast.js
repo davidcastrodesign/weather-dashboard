05bd73a4c11c0df7392340ca5af52a39493d3ebe
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-06 03:05:20 -0700

    modifications to app, forecast, geo

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request');
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=bc2b8570844ca20017843faa12df78ff&query=' + latitude + ',' + longitude + '&units=f';
 
@@ -9,7 +11,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It\'s currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.');
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. Temperature: ' + body.current.temperature + ' degrees. Feels like: ' + 
+                body.current.feelslike + ' degrees out. ' + 'Humidity: ' + body.current.humidity + '%');
+        
         }
     });
 };
